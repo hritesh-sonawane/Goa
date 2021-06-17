4e62d11fc0dbf1434101ef3a766e0a449f9d6d44
Hritesh <hriteshbebop@gmail.com>
2021-06-17 10:02:07 +0530

    add L0 06

---
+++ Level 0/06-ClosestValueInBST/solution2.go
@@ -0,0 +1,37 @@
+package main
+
+type BST struct {
+	Value int
+	Left *BST
+	Right *BST
+}
+
+// Average: O(logN) time | O(1) space
+// Worst: O(N) time | O(1) space
+func (tree *BST) FindClosestValue(target int) int {
+	return tree.findClosestValue(target, tree.Value)
+}
+
+func (tree *BST) findClosestValue(target, closest int) int {
+	currentNode := tree
+	for currentNode != nil {
+		if absdiff(target, closest) > absdiff(target, currentNode.Value) {
+			closest = currentNode.Value
+		}
+		if target < currentNode.Value {
+			currentNode = currentNode.Left
+		} else if target > currentNode.Value {
+			currentNode = currentNode.Right
+		} else {
+			break
+		}
+	}
+	return closest
+}
+
+func absdiff(a, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}

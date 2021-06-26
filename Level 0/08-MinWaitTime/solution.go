25d104dd6a469d3154a39f9d237374df8c8413fc
Hritesh <hriteshbebop@gmail.com>
2021-06-26 13:46:49 +0530

    add L0 08

---
+++ Level 0/08-MinWaitTime/solution.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func MinWaitTime(queries []int) int {
+	sort.Ints(queries)   // [1, 2, 2, 3, 6]
+
+	waitTime := 0
+	for idx, val := range queries {
+		queriesLeft := len(queries) - (idx + 1)
+		waitTime += val * queriesLeft
+	}
+	return waitTime   // 4 + 6 + 4 + 3 = 17
+}
+
+func main() {
+	q := []int{3, 2, 1, 2, 6}
+	fmt.Println(MinWaitTime(q))
+}

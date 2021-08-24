a7bb58d8ee6791c09b554f68cd52ceecf016b76c
Hritesh <hriteshbebop@gmail.com>
2021-08-24 17:28:09 +0530

    add L1 39

---
+++ Level 1/39-YoungestAncestor/solution.go
@@ -0,0 +1,39 @@
+package main
+
+type AncestralTree struct {
+	Name      string
+	Ancestor *AncestralTree
+}
+
+func GetYoungestCommonAncestor(topAncestor, descendantOne, descendantTwo *AncestralTree) *AncestralTree {
+	depthOne := getDescendantDepth(descendantOne, topAncestor)
+	depthTwo := getDescendantDepth(descendantTwo, topAncestor)
+
+	if depthOne > depthTwo {
+		return backtrackAncestralTree(descendantOne, descendantTwo, depthOne-depthTwo)
+	}
+	return backtrackAncestralTree(descendantTwo, descendantOne, depthTwo-depthOne)
+}
+
+func getDescendantDepth(descendant, topAncestor *AncestralTree) int {
+	depth := 0
+
+	for descendant != topAncestor {
+		depth++
+		descendant = descendant.Ancestor
+	}
+	return depth
+}
+
+func backtrackAncestralTree(lowerDescendant, higherDescendant *AncestralTree, diff int) *AncestralTree {
+	for diff > 0 {
+		lowerDescendant = lowerDescendant.Ancestor
+		diff--
+	}
+
+	for lowerDescendant != higherDescendant {
+		lowerDescendant = lowerDescendant.Ancestor
+		higherDescendant = higherDescendant.Ancestor
+	}
+	return lowerDescendant
+}
